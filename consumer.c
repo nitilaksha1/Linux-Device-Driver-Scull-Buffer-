1585066fc57a2d3b2088c9ae95423d70d3e284df
Nitilaksha Halakatti <halak004@umn.edu>
2016-12-01 15:53:07 -0600

    ->Added the producer and consumer code Producer.c takes parms : number of items to produce and item name Consumer.c takes parms : number of items
    These files were written to test the working of producer and consumer with the original scullpipe code (only the buffer size was changed to be a multiple of 32)
    
    Whats working:
    When the producer and consumer are run concurrently the consumer consumes all the items produced
    
    What Fails:
    When producer runs first and exits and then consumer is run, the consumer remains in a blocked state

---
+++ consumer.c
@@ -0,0 +1,71 @@
+#include<errno.h>
+#include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+#define DEBUG 1
+#define ITEM_SIZE 32
+
+int main(int argc, char **argv) {
+
+	int fd, noofitems = 0, res;
+	char * buffer = NULL;
+
+	if (argc != 2) {
+		#ifdef DEBUG
+		printf("\r\nToo few arguments to producer\r\n");
+		#endif
+		return 0;
+	}
+
+	//Open the file in write mode
+	//Which scullbuffer device to open from 0-3????
+	fd = open("/dev/scullpipe0", O_RDONLY);
+
+	if (fd == -1) {
+		#ifdef DEBUG
+		printf("\r\nFailed to open the device %d\r\n", errno);
+		#endif
+		return 0;
+	}
+
+	sleep(3);
+
+	//Allocate an item of size 32 bytes	
+	buffer = (char *)malloc(ITEM_SIZE);
+	
+	noofitems = atoi(argv[1]);
+
+	//Write the required number of items into the scull buffer
+	for (int i = 0; i < noofitems; i++) {
+	
+		#ifdef DEBUG
+		printf("\r\nBefore read call\r\n");
+		#endif
+
+		res = read (fd, buffer, ITEM_SIZE);
+		
+		if (!res) {
+			//Should we exit or continue to write the next item
+		}
+
+		if (res < 0) {
+			//Should we exit??
+		}
+
+		#ifdef DEBUG
+		printf("\r\nThe buffer read is %s\r\n", buffer);
+		#endif
+		memset(buffer, '\0', ITEM_SIZE);
+	}
+
+	//free the buffer
+	cleanup: free(buffer);
+	close(fd);
+
+	return 0;
+}
---
+++ producer.c
@@ -0,0 +1,71 @@
+#include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+#define DEBUG 1
+#define ITEM_SIZE 32
+
+/*The program takes the No of items to be produced and the item string as the parameter*/
+int main(int argc, char **argv) {
+
+	int fd, noofitems = 0, res;
+	char * buffer = NULL;
+
+	if (argc != 3) {
+		#ifdef DEBUG
+		printf("\r\nToo few arguments to producer\r\n");
+		#endif
+		return 0;
+	}
+
+	//Open the file in write mode
+	fd = open("/dev/scullpipe0", O_WRONLY);
+
+	if (fd == -1) {
+		#ifdef DEBUG
+		printf("\r\nFailed to open the device\r\n");
+		#endif
+		return 0;
+	}
+
+	sleep(3);
+
+	//Allocate an item of size 32 bytes	
+	buffer = (char *)malloc(ITEM_SIZE);
+	
+	noofitems = atoi(argv[1]);
+
+	//Write the required number of items into the scull buffer
+	for (int i = 0; i < noofitems; i++) {
+		
+		sprintf(buffer, "%s000%d", argv[2], i);
+
+		#ifdef DEBUG
+		printf("\r\nThe buffer contents are %s\r\n", buffer);
+		#endif
+
+		res = write (fd, buffer, ITEM_SIZE);
+
+		#ifdef DEBUG
+		printf("\r\nThe number of bytes written are %d\r\n", res);
+		#endif
+
+		if (!res)
+			//Should we exit or continue to write the next item
+
+		if (res < 0)
+			//Should we exit??
+		
+		memset(buffer, '\0', ITEM_SIZE);
+	}
+
+	//free the buffer
+	cleanup: free(buffer);
+	close(fd);
+
+	return 0;
+}
