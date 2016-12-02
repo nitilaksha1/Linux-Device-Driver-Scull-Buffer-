13ff2f2ed93fd176dd19f4d54b85d20e44f631ff
Andrew Banman <and.banman@gmail.com>
2016-12-01 18:39:14 -0600

    Add testcase.sh script for testing

---
+++ testcase.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# testcase.sh - simple script to run each of the 5 test cases for scullbuffer
+# Author: Andrew Banman <banma001@umn.edu>, Niti Halakatti <halak004@umn.edu>
+
+case $1 in
+	1) producer 50 red & consumer 50;;
+	2) producer 50 red & consumer 10;;
+	3) producer 50 red & consumer 100;;
+	4) producer 50 red & producer black 50 & consumer 200;;
+	5) producer 50 red & consumer 50 & consumer 50;;
+esac
