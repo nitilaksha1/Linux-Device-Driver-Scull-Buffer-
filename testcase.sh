cf4f70a1a1409e0928c7e6372d5208429f76b459
Andrew Banman <banma001@umn.edu>
2016-12-04 17:02:23 -0600

    update testcase.sh to give better names to logfiles

--- testcase.sh
+++ testcase.sh
@@ -4,9 +4,9 @@
 # Author: Andrew Banman <banma001@umn.edu>, Niti Halakatti <halak004@umn.edu>
 
 case $1 in
-	1) ./producer 50 red & ./consumer 50;;
-	2) ./producer 50 red & ./consumer 10;;
-	3) ./producer 50 red & ./consumer 100;;
-	4) ./producer 50 red & ./producer 50 black & ./consumer 200;;
-	5) ./producer 50 red & ./consumer 50 & ./consumer 50;;
+	1) ./producer 50 case1_red & ./consumer 50 case1;;
+	2) ./producer 50 case2_red & ./consumer 10 case2;;
+	3) ./producer 50 case3_red & ./consumer 100 case3;;
+	4) ./producer 50 case4_red & ./producer 50 case4_black & ./consumer 200 case4;;
+	5) ./producer 50 case5_red & ./consumer 50 case5_1 & ./consumer 50 case5_2;;
 esac
